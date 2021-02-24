2cf86de46eeb5ffcbde9121f346621f7e99c91f5
Kseniya <k.hodakovskaya@mail.ru>
2021-02-24 19:50:52 +0300

    Added date & time into footer

--- frontend/src/components/core/VTBFooter.vue
+++ frontend/src/components/core/VTBFooter.vue
@@ -4,14 +4,33 @@
             app
     >
         <v-spacer/>
+        <div class="blue-grey--text">{{localeDate}}</div>
 
-        <span class="blue-grey--text">ВТБ 2021</span>
+        <!--span class="blue-grey--text">ВТБ 2021</span-->
     </v-footer>
 </template>
 
 <script>
     export default {
-        name: "VTBFooter"
+        name: "VTBFooter",
+
+        data: () => ({
+            date: 1614184739,
+        }),
+
+        computed: {
+            localeDate() {
+                return (new Date(this.date)).toLocaleDateString() + " " + (new Date(this.date)).toLocaleTimeString()
+            },
+        },
+
+        created() {
+            this.intervalId = setInterval(() => this.date = Date.now(), 100);
+        },
+
+       // beforeDestroy() {
+       //     if (this.intervalId) clearInterval(this.intervalId)
+      //  },
     }
 </script>
 
