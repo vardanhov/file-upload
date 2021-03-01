62727953f2c5df1c409783b92065c1611260f64e
Виктория <viktrija-iknnikva@yandex.ru>
2021-03-01 19:00:30 +0300

    Рефакторинг дизайна стартовой страницы. Добавлена функциональность кнопок

--- frontend/src/components/Home.vue
+++ frontend/src/components/Home.vue
@@ -10,14 +10,14 @@
     <v-card>
       <v-img
           :src="cardFirst.src"
-          class="white--text align-end"
-          gradient="to bottom, rgba(0,0,0,.1), rgba(0,0,0,.5)"
+          class="white--text align-end card_img"
+          gradient="to bottom, rgba(0,119,221, .2), rgba(0,119,221, .7)"
           height="200px"
       >
-        <v-card-title v-text="cardFirst.title"></v-card-title>
+        <v-card-title style="font-size: 40px;" v-text="cardFirst.title"></v-card-title>
       </v-img>
 
-      <v-card-actions>
+      <v-card-actions style="background: rgba(0,119,221, .1)">
         <v-spacer></v-spacer>
         <v-btn
             icon
@@ -29,10 +29,13 @@
       <v-expand-transition>
         <div v-show="show">
           <v-divider></v-divider>
-      <v-card-text>
-        Данный инструмент является планировщиком для выдачи прав на загрузку дагов и скриптов. При загрузке файлов
-        owner и group равны имени и группе пользователя.
+
+      <v-card-text style="font-weight: 900; font-size: 12px;">
+        Данный инструмент является планировщиком для выдачи прав на загрузку дагов и скриптов.
+        <br>
+        При загрузке файлов "владелец" и "группа" соответствуют имени и группе пользователя.
       </v-card-text>
+
         </div>
       </v-expand-transition>
     </v-card>
@@ -49,18 +52,20 @@
             <v-img
                 :src="card.src"
                 class="white--text align-end"
-                gradient="to bottom, rgba(0,0,0,.1), rgba(0,0,0,.5)"
+                gradient="to bottom, rgba(0,119,221, .2), rgba(0,119,221, .7)"
                 height="200px"
                 width ="800px"
             >
-              <v-card-title v-text="card.title"></v-card-title>
+              <v-card-title  v-text="card.title"></v-card-title>
             </v-img>
 
-            <v-card-actions>
+            <v-card-actions style="background: rgba(0,119,221, .1)">
               <v-spacer></v-spacer>
               <v-btn
                   color="primary"
                   text
+                  :to="card.url"
+                  :title="card.label"
               >
                 Открыть
               </v-btn>
@@ -80,8 +85,8 @@ export default {
     show: false,
     cardFirst: { title: 'Инструкция', src: '', flex: 11 },
     cards: [
-      { title: 'Загрузка файлов', src: '', flex: 6 },
-      { title: 'Панель управления пользователями', src: '', flex: 6 },
+      { title: 'Загрузка файлов', src: '', flex: 6, label: 'Отправить файл', url: '/upload' },
+      { title: 'Панель управления пользователями', src: '', flex: 6, label: 'Панель управления', url: '/admin' },
     ],
     links: [
       {
